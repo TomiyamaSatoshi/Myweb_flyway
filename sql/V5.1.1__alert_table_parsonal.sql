ca5236326430e525b8e5d647054c534265b47032
tomiyamasatoshi <n2m71212@yahoo.co.jp>
2021-09-02 01:11:04 +0900

    modify parsonal table to add wellcome message name

--- sql/V5.1.1__alert_table_parsonal.sql
+++ sql/V5.1.1__alert_table_parsonal.sql
@@ -1,2 +1,3 @@
 ALTER TABLE parsonal ADD COLUMN wellcome_message_main VARCHAR(512);
 ALTER TABLE parsonal ADD COLUMN wellcome_message_sub VARCHAR(512);
+ALTER TABLE parsonal ADD COLUMN wellcome_message_name VARCHAR(64);
--- sql/V9.1.0__insert_parsonal.sql
+++ sql/V9.1.0__insert_parsonal.sql
@@ -1,1 +1,19 @@
-INSERT INTO parsonal (parsonal_name,birthday,native_place,current_location,hobby,wellcome_message_main,wellcome_message_sub) VALUES ('Satoshi, Tomiyama（富山　悟志）','1993/02/21','Fujiyoshida, Yamanashi','ichikawa, chiba','Running, Have a drink, Traveling, Golf','Thank you for visiting my web page.','My work is System Developer.Please get to know me and take an interest in me.');
+INSERT INTO parsonal (
+    parsonal_name
+    ,birthday
+    ,native_place
+    ,current_location
+    ,hobby
+    ,wellcome_message_main
+    ,wellcome_message_sub
+    ,wellcome_message_name
+) VALUES (
+    'Satoshi, Tomiyama（富山　悟志）'
+    ,'1993/02/21'
+    ,'Fujiyoshida, Yamanashi'
+    ,'ichikawa, chiba'
+    ,'Running, Have a drink, Traveling, Golf'
+    ,'Thank you for visiting my web page.'
+    ,'My work is System Developer.Please get to know me and take an interest in me.'
+    ,'I''m Satoshi, Tomiyama.'
+);
